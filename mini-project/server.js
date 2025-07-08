45cdd4ee8eca1b9ddaf5a9110f742aa52938fa23
rakhi <rakhisingh876.com@gmail.com>
2025-07-08 20:34:20 +0530

    update

--- mini-project/server.js
+++ mini-project/server.js
@@ -1,25 +1,47 @@
 const express = require('express');
 const app = express();
-
-// Built‑in middleware to parse JSON bodies
+let notes = [];
+app.use(express.static('public'));
 app.use(express.json());
 
-app.post('/notes', (req, res) => {
-  // Whatever JSON you send from Postman appears here
-  console.log(req.body);
-  notes.push(res.body)
-  res.json({
-    message: "Note added successfully",
-    notes: note
-  })
-
-  // Echo it back so Postman sees something
-//   res.status(201).json({
-//     message: 'Note received!',
-//     data: req.body
-//   });
- });
-
-app.listen(3002, () => {
-  console.log('Server is running on port 3002');
+app.get('/', (req, res) => {
+    res.send('Hello, RAKHI');
 });
+
+app.get('/notes', (req, res) => {
+    res.send(notes);
+});
+
+app.post('/notes', (req, res) => {
+    console.log(req.body);
+    notes.push(req.body);
+    res.json({
+        message: "note created successfully"
+    });
+});
+app.delete('/notes/:index',(req,res)=>{
+    const index= req.params.index;
+    delete notes[index];
+    res.json({
+        message:"note deleted successfully",
+    })
+})
+
+app.patch('/notes/:index',(req,res)=>{
+    const index = req.params.index;
+    const {title}=req.body
+    notes[index].title=title;
+    res.json({
+        message:"note updated successfully",
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+});
+
+
+
+// GET /notes → returns all saved notes
+
+// POST /notes → adds a new note and confirms with JSON response
